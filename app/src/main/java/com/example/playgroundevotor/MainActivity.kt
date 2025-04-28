a21094b07e2d6cd4be662f6bb69ca74bb0d13f20
Kate OEA <freesyncoea@mail.ru>
2025-04-28 19:25:27 +0700

    added print

--- app/src/main/java/com/example/playgroundevotor/MainActivity.kt
+++ app/src/main/java/com/example/playgroundevotor/MainActivity.kt
@@ -4,10 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.playgroundevotor.databinding.ActivityMainBinding
 import com.example.playgroundevotor.data.Prefs
-import ru.evotor.framework.core.action.event.receipt.changes.position.PositionAdd
+import ru.evotor.framework.component.PaymentPerformer
+import ru.evotor.framework.core.IntegrationManagerCallback
+import ru.evotor.framework.core.IntegrationManagerFuture
+import ru.evotor.framework.core.action.command.print_receipt_command.PrintCorrectionIncomeReceiptCommand
+import ru.evotor.framework.payment.PaymentSystem
+import ru.evotor.framework.payment.PaymentType
 import ru.evotor.framework.receipt.Measure
+import ru.evotor.framework.receipt.Payment
 import ru.evotor.framework.receipt.Position
+import ru.evotor.framework.receipt.PrintGroup
+import ru.evotor.framework.receipt.Receipt
+import ru.evotor.framework.receipt.correction.CorrectionType
 import java.math.BigDecimal
+import java.util.Date
 import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
@@ -22,9 +32,92 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         title = "Тестовое приложение для Evotor"
+
+        binding.printReceiptButton.setOnClickListener {
+            printCorrectionIncome(generateReceipt())
+        }
     }
 
-    private fun positions(): List<PositionAdd> {
+    private fun generateReceipt(): List<Receipt.PrintReceipt> {
+        val printGroup = PrintGroup(
+            UUID.randomUUID().toString(),
+            PrintGroup.Type.CASH_RECEIPT,
+            null,
+            null,
+            null,
+            null,
+            true,
+            null,
+            null
+        )
+
+        val paymentSystem = PaymentSystem(
+            PaymentType.CASH,
+            "cash",
+            "ru.evotor.paymentSystem.cash.base"
+        )
+        val performer = PaymentPerformer(paymentSystem, null, null, null, PaymentType.CASH.name)
+
+
+        val payments = HashMap<Payment, BigDecimal>()
+        payments[Payment(
+            UUID.randomUUID().toString(),
+            BigDecimal.TEN,
+            null,
+            performer,
+            null,
+            null,
+            UUID.randomUUID().toString()
+        )] = BigDecimal.TEN
+
+
+        return listOf(
+            Receipt.PrintReceipt(
+                printGroup,
+                positions = positions(),
+                discounts = null,
+                payments = payments, changes = mapOf()
+            )
+        )
+    }
+
+    private fun printCorrectionIncome(
+        printReceipt: List<Receipt.PrintReceipt>,
+    ) {
+
+        val managerCallback = IntegrationManagerCallback { future ->
+            try {
+                val result = future.result
+
+                if (result.type == IntegrationManagerFuture.Result.Type.ERROR) {
+                    notifyUser("Ошибка формирования чека: " + result.error.message + ". Код: " + result.error.code)
+                    return@IntegrationManagerCallback
+                } else {
+
+                }
+            } catch (e: Exception) {
+                notifyUser("Exception: ${e.localizedMessage}")
+            } finally {
+            }
+        }
+
+        PrintCorrectionIncomeReceiptCommand(
+            printReceipts = printReceipt,
+            extra = null,
+            clientPhone = null,
+            clientEmail = null,
+            receiptDiscount = null,
+            paymentAddress = null,
+            paymentPlace = null,
+            userUuid = null,
+            correctionDate = Date(),    /* 1178 */
+            correctionType = CorrectionType.BY_SELF,
+            prescription = null
+        ).process(this, managerCallback)
+    }
+
+
+    private fun positions(): List<Position> {
         val position = Position.Builder.newInstance(
             UUID.randomUUID().toString(),
             null,
@@ -37,7 +130,7 @@ class MainActivity : AppCompatActivity() {
             BigDecimal.TEN,
             BigDecimal.ONE
         ).build()
-        return listOf(PositionAdd(position))
+        return listOf((position))
     }
 
     override fun onResume() {
